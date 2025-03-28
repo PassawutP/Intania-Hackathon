1db042be1972d7d74c6ed9eb30d9b40a0bb91761
Passawut <passawut.prinyagul@gmail.com>
2025-03-28 22:20:18 +0700

    feat: add basic connection to mysql in docker

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,48 @@
 import express, { Request, Response } from 'express';
+import mysql from 'mysql2';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000; 
+
+console.log(process.env.MYSQL_USER);
+console.log(process.env.MYSQL_PASSWORD);
+console.log(process.env.MYSQL_HOST);
+console.log(process.env.MYSQL_PORT);
+console.log(process.env.MYSQL_DATABASE);
+
+
+const db = mysql.createConnection({
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+  port: parseInt(process.env.MYSQL_PORT || '3307', 10),
+});
+
+db.connect((err) => {
+  if (err) {
+    console.error('Error connecting to MySQL:', err.stack);
+    return;
+  }
+  console.log('Connected to MySQL as ID ' + db.threadId);
+});
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello, Express with TypeScript!');
+});
+
+
+app.get('/users', (req, res) => {
+  db.query('SELECT * FROM users', (err, results) => {
+    if (err) {
+      res.status(500).send({ message: 'Database query failed', error: err });
+    } else {
+      res.json(results);
+    }
+  });
 });
 
 app.listen(port, () => {
